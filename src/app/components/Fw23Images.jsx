40412f5af40a75e9a5d009e57232a737001a5268
Joseph Wilde <jwilde19@gmail.com>
2023-10-09 23:32:39 -0400

    Add all images

---
+++ src/app/components/Fw23Images.jsx
@@ -0,0 +1,329 @@
+import Image from 'next/image'
+
+export default function Fw23Images() {
+  const imgWidth = 300
+  const imgHeight = 100
+
+  return (
+    <div className='grid grid-cols-3 grid-rows-1 align-middle'>
+      <Image
+        src='/fw23/FW23_LOOKBOOK_01_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_01_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_02_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_03_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_03_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_04_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_04_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_04_3.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_05_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_05_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_06_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_06_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />{' '}
+      <Image
+        src='/fw23/FW23_LOOKBOOK_07_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />{' '}
+      <Image
+        src='/fw23/FW23_LOOKBOOK_07_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />{' '}
+      <Image
+        src='/fw23/FW23_LOOKBOOK_08_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />{' '}
+      <Image
+        src='/fw23/FW23_LOOKBOOK_08_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />{' '}
+      <Image
+        src='/fw23/FW23_LOOKBOOK_09_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />{' '}
+      <Image
+        src='/fw23/FW23_LOOKBOOK_09_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />{' '}
+      <Image
+        src='/fw23/FW23_LOOKBOOK_09_3.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />{' '}
+      <Image
+        src='/fw23/FW23_LOOKBOOK_10_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_11_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_12_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_13_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_14_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_15_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_16_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_17_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_18_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_19_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_20_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_21_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_22_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_23_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_23_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_20_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_24_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_26_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_26_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_26_3.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_27_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_27_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_28_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_28_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_29_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_29_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_30_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_31_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_31_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_32_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_33_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_33_2.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_34_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+      <Image
+        src='/fw23/FW23_LOOKBOOK_35_1.webp'
+        alt='FW23 Lookbook Image'
+        width={imgWidth}
+        height={imgHeight}
+      />
+    </div>
+  )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,17 @@
+import Image from 'next/image'
+import Fw23Images from './components/Fw23Images'
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>44 Label Group</h1>
+    <main className='flex min-h-screen flex-col items-center justify-between bg-[#f5f2f7]'>
+      <Image
+        src='/44_LOGO_transparent.avif'
+        alt='logo'
+        width={340}
+        height={200}
+        className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-100 hover:opacity-5'
+      />
+      <Fw23Images />
     </main>
   )
 }
