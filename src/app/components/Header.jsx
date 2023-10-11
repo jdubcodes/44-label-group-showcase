53755f672e27189b0093b3303b4d8f51f46d507c
Joseph Wilde <jwilde19@gmail.com>
2023-10-10 23:20:39 -0400

    Create header outline

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className='flex w-screen justify-between'>
+    <header className='flex w-full justify-evenly p-4'>
       <nav>
-        <ul className='flex'>
+        <ul className='flex gap-10 grow'>
           <li>
             <Link href='/' className='text-black'>
               FW23 | BLAME SOCIETY
@@ -15,14 +15,18 @@ export default function Header() {
               SS23 | FULL OF FIRE
             </Link>
           </li>
+          <div className='bg-[url("/44_LOGO_transparent.avif")] w-[340px] h-[50px] bg-no-repeat'></div>
+          <li>
+            <Link href='/' className='text-black'>
+              FW22 | SCREWED UP
+            </Link>
+          </li>
+          <li>
+            <Link href='/' className='text-black'>
+              SS22 | EVERY DARK NIGHT
+            </Link>
+          </li>
         </ul>
-      </nav>
-      <div>
-        <div className='bg-[url("/44_LOGO_transparent.avif")]'></div>
-      </div>
-      <nav>
-        <Link href='/'>FW22 | SCREWED UP</Link>
-        <Link href='/'>2222 | EVERY DARK NIGHT</Link>
       </nav>
     </header>
   )
